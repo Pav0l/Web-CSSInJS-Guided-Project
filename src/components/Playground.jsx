16d06384a4e1dbbbaf85b4c55255939a63b24e5e
Pav0l <pavol.karas@gmail.com>
2019-02-21 12:53:07 +0100

    end of lecture

--- src/components/Playground.jsx
+++ src/components/Playground.jsx
@@ -5,6 +5,22 @@ import styled from 'styled-components';
 const StyledHeader = styled.header`
   border: 4px solid ${pr => pr.primary};
   padding: 10px;
+  @media (max-width: 400px) {
+    &{
+      border: 4px solid ${pr => pr.primary};
+    }
+  }
+  nav {
+    display: flex;
+    justify-content: center;
+    a {
+      margin-left: 10px;
+      margin-right: 10px;
+      &:hover {
+        color: yellow;
+      }
+    }
+  }
 `;
 
 export default function Playground({ primary }) {
